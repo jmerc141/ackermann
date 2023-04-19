75806e89fe90d824a42c096541cc62f8bef6b5fc
jmerc141 <james@DESKTOP-G75PMB9>
2023-04-19 16:21:10 -0400

    fixed ackcheap

--- python/ackcheap.py
+++ python/ackcheap.py
@@ -1,6 +1,26 @@
 #ackcheap
+import sys
 
-return n + 1
-return n + 2
-return 2n + 3
-return 2^n+3 -3
+def ackcheap(m, n):
+	if m == 0:
+		return n + 1
+	elif m == 1:
+		return n + 2
+	elif m == 2:
+		return (2 * n) + 3
+	elif m == 3:
+		return 2**(n + 3) - 3
+	elif m == 4: 
+		return (2**2**2**2**n) - 3
+	elif m == 5:
+		return (2**2**2**2**2**n) - 3
+
+
+if __name__ == "__main__":
+	m = int(sys.argv[1])
+	n = int(sys.argv[2])
+
+	# 20,000 digits
+	sys.set_int_max_str_digits(20000)
+
+	print(ackcheap(m, n))
