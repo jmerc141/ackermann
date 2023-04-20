bc2fae16b0088ea1d283f9f74e1c0435a7cda409
jmerc141 <james@DESKTOP-G75PMB9>
2023-04-20 15:10:34 -0400

    still fixing python/cheap, added c cheap

--- c/recursive/64-bit/compileack.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-del ack.exe
-gcc -Wall -g C:\Users\james\Desktop\sandbox\ackerman\c\ack.c -o C:\Users\james\Desktop\sandbox\ackerman\c\ack.exe -lgmp
-echo.
--- python/ackcheap.py
+++ python/ackcheap.py
@@ -1,5 +1,6 @@
 #ackcheap
 import sys
+from sys import platform
 
 def ackcheap(m, n):
 	if m == 0:
@@ -11,16 +12,29 @@ def ackcheap(m, n):
 	elif m == 3:
 		return 2**(n + 3) - 3
 	elif m == 4: 
-		return (2**2**2**2**n) - 3
+		return a4(n)
 	elif m == 5:
 		return (2**2**2**2**2**n) - 3
+	elif m == 6:
+		return 0
 
+# try njit
+def a3(n):
+	return (2 * n) + 3
+
+def a4(n):
+	return (2**2**2**2**n) - 3
 
 if __name__ == "__main__":
+	if platform == "win32":
+		# 20,000 digits
+		sys.set_int_max_str_digits(20000)
+
 	m = int(sys.argv[1])
 	n = int(sys.argv[2])
 
-	# 20,000 digits
-	sys.set_int_max_str_digits(20000)
+	if platform == "win32":
+		# 20,000 digits
+		sys.set_int_max_str_digits(20000)
 
 	print(ackcheap(m, n))
