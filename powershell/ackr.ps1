39ee8ac850d855c83e6df5e9bcea064fa2babab2
jmerc141 <james@OMC-PR-03>
2023-05-26 13:31:55 -0400

    added batch stack and powershell version

---
+++ powershell/ackr.ps1
@@ -0,0 +1,16 @@
+function Get-Ackermann ([int64]$m, [int64]$n)
+{
+    if ($m -eq 0)
+    {
+        return $n + 1
+    }
+ 
+    if ($n -eq 0)
+    {
+        return Get-Ackermann ($m - 1) 1
+    }
+ 
+    return (Get-Ackermann ($m - 1) (Get-Ackermann $m ($n - 1)))
+}
+
+Get-Ackermann $args[0] $args[1]
