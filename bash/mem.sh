2042e01330174b0b4fbb4ee7edf6f81e60d06795
jmerc141 <james.merc@yahoo.com>
2023-12-20 20:31:58 -0500

    fixed bash mem version

--- bash/mem.sh
+++ bash/mem.sh
@@ -2,36 +2,32 @@
 
 #!/bin/bash
 
-declare -A ack
-#ack[$1$2]="hello"
-m=$1
-n=$2
+declare -A cache
 
 amem(){
-	#echo $1 $2
-	if [[ -n "${ack[$1$2]}" ]]
+	local m=$1
+	local n=$2
+
+	if [[ -n ${cache[$1$2]} ]]
 	then
-		echo here
-	else
-		#echo s
-		if [[ $1 -eq 0 ]]
-		then
-			#echo one
-			ack[$1$2]=$(($2+1))
-		elif [[ $2 -eq 0 ]]
-		then
-			#echo two
-			ack[$1$2]=$(amem $(($1-1)) 1)
-		else
-			#echo three
-			tmp=$(amem $(($m-1)) $(amem $m $(($n-1))) )
-			ack[$1$2]=$tmp
-		fi
+		echo ${cache["$m,$n"]}
+		return
 	fi
-	echo ${ack[$1$2]}
-	#return "${ack[$1 $2]}"
-	
+
+	if [[ $m -eq 0 ]]
+	then
+		#echo one
+		result=$(($n+1))
+	elif [[ $n -eq 0 ]]
+	then
+		result=$(amem $(($m-1)) 1)
+	else
+		result=$(amem $(($m-1)) $(amem $m $(($n-1))) )
+	fi
+
+	cache["$m,$n"]=$result
+
+	echo $result
 }
 
 amem $1 $2
-echo ${!ack[@]}: ${ack[@]}
